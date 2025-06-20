fb006d221d4cf11870dd0bf4258094cbb526b99c
Ksaikiran28 <ksaikiran1469@gmail.com>
2025-06-20 22:00:19 +0530

    ug

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,23 +1,12 @@
 import mongoose from "mongoose";
-
-export const connectDB = async () => {
-    try {
-      const uri = process.env.MONGO_URI;
-      console.log("Trying to connect to MongoDB with URI:", uri);
-  
-      if (!uri) {
-        throw new Error("MONGO_URI is not defined");
-      }
-  
-      await mongoose.connect(uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
-  
-      console.log("MONGODB CONNECTED SUCCESSFULLY");
-    } catch (error) {
-      console.error("❌ Error connecting to MongoDB:", error.message);
-      process.exit(1);
+export const connectDB = async()=>{
+    try{
+        await mongoose.connect(
+            process.env.MONGO_URI
+        );
+        console.log("MONGODB CONNECTED SUCCESSFULLY")
+    }catch(error){
+        console.error("Error connecting to MONGODB",error);
+        process.exit(1); //exit means error
     }
-  };
-  
+}
